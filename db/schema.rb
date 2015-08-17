e3e3561ef700ae28c5ab262efb8fb482b5514537
floalex <flo.liang@my.com>
2015-08-17 18:50:11 +0000

    Associate Reviews with Restaurants

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150814215527) do
+ActiveRecord::Schema.define(version: 20150817173433) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
@@ -26,10 +26,13 @@ ActiveRecord::Schema.define(version: 20150814215527) do
   create_table "reviews", force: :cascade do |t|
     t.integer  "rating"
     t.text     "comment"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
     t.integer  "user_id"
+    t.integer  "restaurant_id"
   end
+
+  add_index "reviews", ["restaurant_id"], name: "index_reviews_on_restaurant_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
